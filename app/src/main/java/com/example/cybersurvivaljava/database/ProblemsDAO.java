bbdee6d37782efcd6845e69e062aeb943dc7fb07
lopezjesuspersonal <lopezjesus.personal@gmail.com>
2025-12-01 21:31:48 -0800

    Fixed gradle, can now write default entries for Problems table

--- app/src/main/java/com/example/cybersurvivaljava/database/ProblemsDAO.java
+++ app/src/main/java/com/example/cybersurvivaljava/database/ProblemsDAO.java
@@ -3,6 +3,7 @@ package com.example.cybersurvivaljava.database;
 import androidx.room.Dao;
 import androidx.room.Insert;
 import androidx.room.OnConflictStrategy;
+import androidx.room.Query;
 
 import com.example.cybersurvivaljava.database.entities.Problems;
 
@@ -11,4 +12,7 @@ public interface ProblemsDAO {
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
     void insert(Problems problem);
+
+    @Query("SELECT COUNT(*) FROM " + CyberSurvivalDatabase.PROBLEMS_TABLE_NAME)
+    int count();
 }
