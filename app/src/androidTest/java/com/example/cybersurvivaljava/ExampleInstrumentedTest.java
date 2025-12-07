bd5405b88571e0ef221af12329d9cce6ef13c9e8
lopezjesuspersonal <lopezjesus.personal@gmail.com>
2025-12-06 22:46:48 -0800

    Added my 2 tests

--- app/src/androidTest/java/com/example/cybersurvivaljava/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/cybersurvivaljava/ExampleInstrumentedTest.java
@@ -1,12 +1,20 @@
 package com.example.cybersurvivaljava;
 
 import android.content.Context;
-
-import androidx.test.platform.app.InstrumentationRegistry;
+import androidx.room.Room;
+import androidx.test.core.app.ApplicationProvider;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
+import com.example.cybersurvivaljava.database.CyberSurvivalDatabase;
+import com.example.cybersurvivaljava.database.UserDAO;
+import com.example.cybersurvivaljava.database.entities.User;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+
+import java.io.IOException;
 
 import static org.junit.Assert.*;
 
@@ -17,10 +25,43 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    private UserDAO userDao;
+    private CyberSurvivalDatabase db;
+
+    @Before
+    public void createDb() {
+        Context context = ApplicationProvider.getApplicationContext();
+        db = Room.inMemoryDatabaseBuilder(context, CyberSurvivalDatabase.class)
+                .allowMainThreadQueries()
+                .build();
+        userDao = db.userDAO();
+    }
+
+    @After
+    public void closeDb() throws IOException {
+        db.close();
+    }
+
     @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getInstrumentation().getTargetContext();
-        assertEquals("com.example.cybersurvivaljava", appContext.getPackageName());
+    public void createUser() throws Exception {
+        User user = new User("testuser", "password");
+        userDao.insert(user);
+        User byName = userDao.findUserByUsername_TEST("testuser");
+        assertEquals(user.getUsername(), byName.getUsername());
+    }
+
+    @Test
+    public void deleteUser() throws Exception {
+        User user = new User("deleteMe", "password");
+        userDao.insert(user);
+
+        User insertedUser = userDao.findUserByUsername_TEST("deleteMe");
+        assertNotNull(insertedUser);
+
+        userDao.delete(insertedUser);
+
+        User deletedUser = userDao.findUserByUsername_TEST("deleteMe");
+        assertNull(deletedUser);
     }
 }
